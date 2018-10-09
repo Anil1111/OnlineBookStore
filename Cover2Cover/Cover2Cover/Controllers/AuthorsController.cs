862d86a9bac9d48f13ad72845b04ba2d9d8ad775
Mostafa Abdelrahman <43328278+Mustafa-Abdelrahman@users.noreply.github.com>
2018-10-09 18:34:43 +0200

    Adding new controllers (Books and Authors) with views

---
+++ Cover2Cover/Cover2Cover/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using Cover2Cover.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cover2Cover.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AuthorsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Authors
+        public ActionResult Index()
+        {
+            var authors = _context.Authors.Include(b => b.Books).ToList();
+            return View(authors);
+        }
+        public ActionResult Details(int id)
+        {
+            var author = _context.Authors.SingleOrDefault(a => a.AuthorId == id);
+            if (author != null)
+            {
+                return View(author);
+
+            }
+            else return HttpNotFound();
+        }
+    }
+}
---
+++ Cover2Cover/Cover2Cover/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using Cover2Cover.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cover2Cover.Controllers
+{
+    public class BooksController : Controller
+    {
+        private ApplicationDbContext _context;
+        public BooksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Books
+        public ActionResult Index()
+        {
+            var books = _context.Books.Include(b => b.Author).ToList() ;
+            return View(books);
+        }
+        public ActionResult Details(int id)
+        {
+            var book = _context.Books.Include(a => a.Author).SingleOrDefault(b => b.Id == id);
+            if (book !=null)
+            {
+                return View(book);
+
+            }
+            else return HttpNotFound();
+            
+        }
+    }
+}
